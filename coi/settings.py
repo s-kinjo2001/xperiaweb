74201c77a1f3bfc14168144d60248af8adc64cae
Seima Kinjo <s.kinjo@asup.co.jp>
2023-11-02 18:00:00 +0900

    ALLOWED_HOSTSに.onrender.comを追加

--- coi/settings.py
+++ coi/settings.py
@@ -15,9 +15,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.onrender.com']
 
 
 # Application definition
